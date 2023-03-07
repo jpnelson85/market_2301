4b186985cf1bcb48a50c3f3bbdee820cf164dbf0
Jeff Nelson <jpnelson85@gmail.com>
2023-03-07 10:29:30 -0600

    add to iteration 1

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,11 @@
 class Vendor
-  
+  attr_reader :name,
+              :inventory,
+              :check_stock
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+    @check_stock = 0
+  end
 end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,8 +3,27 @@ require './lib/vendor'
 
 RSpec.describe Vendor do
   it 'exists' do
-    vendor = Vendor.new
+    vendor = Vendor.new("Rocky Mountain Fresh")
     expect(vendor).to be_a(Vendor)
+  end
+
+  it 'has a name and array of inventory and check stock' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    expect(vendor.name).to eq("Rocky Mountain Fresh")
+    expect(vendor.inventory).to eq({})
+    expect(vendor.check_stock).to eq(0)
+  end
+
+  it 'can stock inventory' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    ventor.stock(item1, 30)
+    expect(vendor.check_stock(item1)).to eq(30)
+
+
   end
 end
 
