3f635abeb11d30b97abf2d59ccdc655c27f2f17b
Jeff Nelson <jpnelson85@gmail.com>
2023-03-07 11:17:12 -0600

    update iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -1,7 +1,13 @@
 class Market
-  attr_reader
+  attr_reader :name,
+              :vendors
 
   def initialize(name)
     @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
   end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -7,4 +7,32 @@ RSpec.describe Market do
     market = Market.new("South Pearl Street Farmers Market") 
     expect(market).to be_a(Market)
   end
+
+  it 'has empty array of vendors' do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    market = Market.new("South Pearl Street Farmers Market") 
+    expect(market.vendors).to eq([])
+  end
+
+  it 'add vendors' do
+    market = Market.new("South Pearl Street Farmers Market") 
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack") 
+    market.add_vendor(vendor1)
+    expect(market.vendors).to eq([vendor1])
+    market.add_vendor(vendor2)
+    expect(market.vendors).to eq([vendor1, vendor2])
+    market.add_vendor(vendor3)
+    expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+  end
+
 end
+
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack") 
